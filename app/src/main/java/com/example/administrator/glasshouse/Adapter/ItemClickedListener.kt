f288368ac5898d8ab319ca042500012f7a7e4efb
QuangDuy <buiquangduy18@gmail.com>
2018-10-21 18:40:21 +0700

    thirdUpdate
    update tính năng hiện dialog

--- app/src/main/java/com/example/administrator/glasshouse/Adapter/ItemClickedListener.kt
+++
@@ -1,7 +0,0 @@
-package com.example.administrator.glasshouse.Adapter
-
-import android.view.View
-
-interface ItemClickedListener {
-    fun onClick(view: View, position: Int, isLongClick: Boolean)
-}
--- app/src/main/java/com/example/administrator/glasshouse/Adapter/RecyclerOverviewAdapter.kt
+++ app/src/main/java/com/example/administrator/glasshouse/Adapter/RecyclerOverviewAdapter.kt
@@ -1,6 +1,6 @@
 package com.example.administrator.glasshouse.Adapter
 
-import android.app.AlertDialog
+import android.app.Dialog
 import android.content.Context
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
@@ -10,13 +10,13 @@ import android.widget.*
 import com.example.administrator.glasshouse.ModelTest.Datacheck_1
 import com.example.administrator.glasshouse.R
 
-class RecyclerOverviewAdapter(val dataList : ArrayList<Datacheck_1>,val context: Context) : RecyclerView.Adapter<RecyclerOverviewAdapter.ViewHolder>(){
-    public lateinit var itemClickListener : ItemClickedListener
+
+class RecyclerOverviewAdapter(val dataList: ArrayList<Datacheck_1>, val context: Context) : RecyclerView.Adapter<RecyclerOverviewAdapter.ViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val layoutInflater = LayoutInflater.from(parent.context)
-        val itemView = layoutInflater.inflate(R.layout.item_row, parent, false)
-        return ViewHolder(itemView)
+        val view = layoutInflater.inflate(R.layout.item_row, parent, false)
+        return ViewHolder(view)
     }
 
     override fun getItemCount(): Int {
@@ -30,44 +30,61 @@ class RecyclerOverviewAdapter(val dataList : ArrayList<Datacheck_1>,val context:
         holder.txtPin.text = dataList[position].pin.toString()
         holder.progessbar.progress = dataList[position].pin
         holder.txtNodeName.text = dataList[position].nodeName
-        holder.setNode.setOnClickListener {it ->
-            showSetNodeDialog(context,holder)
-            }
+        holder.setNode.setOnClickListener {
+            showSetNodeDialog(context, holder)
         }
+        holder.itemView.setOnClickListener {
+            setRelayDialog(context)
+        }
+    }
 
-    private fun showSetNodeDialog(context: Context,holder:ViewHolder) {
-        val dialogBuilder = AlertDialog.Builder(context).create()
-        val inflater : LayoutInflater = LayoutInflater.from(context)
-        val thisDialog = inflater.inflate(R.layout.custom_dialog_node,null)
-        //thisDialog.setContentView(R.layout.custom_dialog_node)
-        val btnCancel = thisDialog.findViewById<Button>(R.id.btnCancel)
-        val btnOk = thisDialog.findViewById<Button>(R.id.btnOk)
-        val edtSetNode = thisDialog.findViewById<EditText>(R.id.edtSetNode)
+    private fun setRelayDialog(context: Context) {
+        val dialog = Dialog(context)
+        dialog.setContentView(R.layout.custom_dialog_role)
+        val btnRelayOk = dialog.findViewById<View>(R.id.btnRelayOk) as Button
+        val btnRelayCancel = dialog.findViewById<View>(R.id.btnRelayCancel) as Button
+        dialog.setCancelable(true)
+        dialog.create()
+        btnRelayOk.setOnClickListener {
+            dialog.dismiss()
+        }
+        btnRelayCancel.setOnClickListener {
+            dialog.dismiss()
+        }
+        dialog.show()
+
+        // Set các sự kiện sau
+    }
+
+    private fun showSetNodeDialog(context: Context, holder: ViewHolder) {
+        //Toast.makeText(context,"setNode Clicked",Toast.LENGTH_LONG).show()
+        val dialog = Dialog(context)
+        dialog.setContentView(R.layout.custom_dialog_node)
+        val btnCancel = dialog.findViewById<Button>(R.id.btnCancel)
+        val btnOk = dialog.findViewById<Button>(R.id.btnOk)
+        val edtSetNode = dialog.findViewById<EditText>(R.id.edtSetNode)
         btnOk.setOnClickListener {
             val nodeName = edtSetNode.text.toString()
             holder.txtNodeName.text = nodeName
-            Toast.makeText(context,"Đổi tên Node thành công",Toast.LENGTH_SHORT).show()
-            dialogBuilder.dismiss()
+            Toast.makeText(context, "Đổi tên Node thành công", Toast.LENGTH_SHORT).show()
+            dialog.dismiss()
         }
         btnCancel.setOnClickListener {
-            dialogBuilder.dismiss()
+            dialog.dismiss()
+        }
+        dialog.show()
     }
-}
 
 
-    inner class ViewHolder(val item : View) : RecyclerView.ViewHolder(item) {
+    inner class ViewHolder(val item: View) : RecyclerView.ViewHolder(item) {
+
         val txtNodeName = item.findViewById<View>(R.id.txtNodeName) as TextView
         val txtTemp = item.findViewById<View>(R.id.txtTemp) as TextView
         val txtDoAm = item.findViewById<View>(R.id.txtDoAm) as TextView
         val txtLight = item.findViewById<View>(R.id.txtLight) as TextView
         val txtPin = item.findViewById<View>(R.id.txtProgress) as TextView
         val progessbar = item.findViewById<View>(R.id.progressBar) as ProgressBar
-        val setNode : Button = item.findViewById<View>(R.id.btnSetDevice) as Button
+        val setNode: Button = item.findViewById<View>(R.id.btnSetDevice) as Button
 
-
-//        fun setItemClickListener(itemClickListener : ItemClickedListener)
-//        {
-//            this.itemClickListener = itemClickListener
-//        }
     }
 }
